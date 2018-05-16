b8a7d834101b3548483cd7dba639a00328144767
hectorborges <hectorborges94@gmail.com>
2018-05-16 15:30:28 -0500

    Rotation code

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -15,19 +15,58 @@ public class CameraRotation : MonoBehaviour {
     Quaternion cameraRotation;
     Transform myCamera;
 
+    Vector3 targetRotation;
+    Vector3 currentRotation;
+
     private void Start()
     {
         myCamera = Camera.main.transform;
+
+        targetRotation = Random.rotation.eulerAngles;
+        targetRotation.z = 0;
     }
 
-    void Update ()
+    private void Update()
     {
-        xRotationValue += Input.GetAxis("Pitch") * (rotationSpeed * .5f) * Time.deltaTime;
-        yRotationValue += Input.GetAxis("Yaw") * (rotationSpeed * .5f) * Time.deltaTime;
-        zRotationValue += Input.GetAxis("Roll") * rotationSpeed * Time.deltaTime;
+     //   if(CheckRotation()) //||
+     //   {
+     //       targetRotation = new Vector3(Random.Range(-pitchClampValue, pitchClampValue), Random.Range(-yawClampValue, yawClampValue), 0);
+     //       targetRotation.z = 0;
+     ////       print("Target Rotation" + targetRotation);
+     //   }
 
-        xRotationValue = ClampAngle(xRotationValue, -pitchClampValue, pitchClampValue);
-        yRotationValue = ClampAngle(yRotationValue, -yawClampValue, yawClampValue);
+    //    myCamera.transform.Rotate(targetRotation.x * Vector3.right * rotationSpeed * Time.deltaTime);
+     //   myCamera.transform.Rotate(targetRotation.y * Vector3.up * rotationSpeed * Time.deltaTime);
+
+    //    myCamera.transform.rotation = Quaternion.Euler(Vector3.Lerp(currentRotation, targetRotation, rotationSpeed * Time.deltaTime));
+
+     //   print("Current Rotation" + currentRotation);
+        //Move(currentRotation.x, currentRotation.y);
+
+    }
+
+    bool CheckRotation()
+    {
+        if (Mathf.Abs(myCamera.transform.rotation.x - targetRotation.x) < .1f)
+        {
+            if (Mathf.Abs(myCamera.transform.rotation.y - targetRotation.y) < .1f)
+            {
+                return true;
+            }
+            else return false;
+        }
+        else
+            return false;
+    }
+
+    void Move (float moveX, float moveY)
+    {
+      //  xRotationValue += moveX * rotationSpeed * Time.deltaTime;
+      //  yRotationValue += moveY * rotationSpeed * Time.deltaTime;
+       // zRotationValue += Input.GetAxis("Roll") * rotationSpeed * Time.deltaTime;
+
+        xRotationValue = ClampAngle(moveX, -pitchClampValue, pitchClampValue);
+        yRotationValue = ClampAngle(moveY, -yawClampValue, yawClampValue);
 
         cameraRotation = Quaternion.Euler(xRotationValue, yRotationValue, zRotationValue);
         myCamera.transform.localRotation = cameraRotation;
